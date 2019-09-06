96f9deef54821dc4d39ff39ded001c533696274a
chingsley <eneja.kc@gmail.com>
2019-09-06 15:41:37 +0100

    - implementation to create register a student - students can edit their profile - refactor view to use partials for form and error messages - implement before_action in students controller to prevent code repetition

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,48 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      # redirect_to root_path
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updatated your profile"
+      #redirect_to student_path(@student)
+      redirect_to @student # This is a short form of the line immediately above
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# everything you make an update to this file (environment.rb) I will recommend restarting the server
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
